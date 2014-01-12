59a70aab6f203f93a8236e4297bfd4b008b1ef4e
Paul B <gate88@gmail.com>
2014-01-12 19:02:35 +0000

    Changes to fix heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109120139) do
+ActiveRecord::Schema.define(version: 20140111172820) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "pages", force: true do |t|
     t.string   "name"
@@ -49,6 +52,11 @@ ActiveRecord::Schema.define(version: 20140109120139) do
   end
 
   add_index "panels", ["page_id"], name: "index_panels_on_page_id", using: :btree
+
+  create_table "products", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "projects", force: true do |t|
     t.string   "name"
